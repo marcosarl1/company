df87aed3247639594e40e424d7ebc43ad3a854a5
Marcos Júnior <marcosajr11@gmail.com>
2024-12-18 21:40:13 -0300

    MINOR: Unused import

--- src/main/java/com/company/controller/EmployeeController.java
+++ src/main/java/com/company/controller/EmployeeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:63342")
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
